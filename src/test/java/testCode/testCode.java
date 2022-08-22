80251865498a2e80a2ed8f1fbfcc7bdf1e241d9b
GodOfHammer <52759484+tuanvynguyen1@users.noreply.github.com>
2022-08-22 14:14:22 +0700

    Update testCode.java

--- src/test/java/testCode/testCode.java
+++ src/test/java/testCode/testCode.java
@@ -8,6 +8,6 @@ import java.util.HashMap;
 public class testCode extends BaseTest {
     @Test(dataProvider = "dp")
     public void Test(HashMap<String, String> hashMap){
-        System.out.println(hashMap.get("username"));
+        System.out.println(hashMap.get("username")+"11");
     }
 }
